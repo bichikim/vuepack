6f79b0ff30e75efda94c07246ecfc4c79960d6c1
Bichi Kim <bichi@live.co.kr>
2018-02-04 21:13:33 +0900

    update

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@
 
 import chalk from 'chalk'
 import minimist from 'minimist'
-import webpack from 'webpack'
+//import webpack from 'webpack'
 import getWebpackConfig from './webpack.config'
 const {argv}: {argv: string[]} = process
 const [engineName, what, ...others]: string[] = argv
@@ -17,9 +17,10 @@ console.log(chalk.grey('Running a ') + chalk.green(what))
 const args = minimist(others, {
   alias: {
     c: 'config',
+    d: 'development',
   },
 })
 const configPath: string = args.config || './vuepack.config.js'
-
-const webpackConfig = getWebpackConfig(require(configPath))
-console.log(configPath, others[0], others[1])
+const {development = false} = args
+const webpackConfig = getWebpackConfig(require(configPath), development)
+console.log(webpackConfig, others[0], others[1])
